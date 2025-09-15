c7633d0ea997a972422f544f605721019832c708
rawataman19 <rawataman481@gmail.com>
2025-09-15 21:07:18 +0530

    fixed errors

--- Backend/index.js
+++ Backend/index.js
@@ -119,11 +119,12 @@ socket.on("runCode", async({ roomId, code, language,version }) => {
 const port = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, 'frontend/dist')));
-
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
+app.use(express.static(path.join(__dirname, 'Frontend/dist')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Frontend/dist/index.html'));
 });
+
+
 
 
 
